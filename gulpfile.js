82bff8c5dc0a4f925b0e18efa01e80394a20319d
Brayan Styvison <99987766+BStyvison@users.noreply.github.com>
2022-08-07 21:13:52 -0300

    up

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass') (require('sass'));
+
+function compilaSass() {
+    return gulp
+    .src('css/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+}
+
+gulp.task('default', compilaSass);
+
